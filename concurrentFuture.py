5f2c58153d8b0ec7066cd1e87d6cf142fc4354a8
takomayowasabi <66776472+takomayowasabi@users.noreply.github.com>
2020-09-25 16:27:23 +0900

    Update concurrentFuture.py

--- concurrentFuture.py
+++ concurrentFuture.py
@@ -2,12 +2,22 @@ import time
 import concurrent.futures
 
 def sum(a,b):
-    total=0
+    total=1
     for i in range(a,b):
-        total+=i
+        total = total + i
+    print(time.time()-time1)
     return total
- 
+print("직렬 처리 시간:")
+
 time1=time.time()
+
+sum(1,10000000)
+sum(1,10000000)
+
+print()    
+print("병렬 처리 시간:")
+time1=time.time()
+
 with concurrent.futures.ThreadPoolExecutor(max_workers=i) as executor:
     future = executor.submit(sum, 1, 10000000)
-    print(time.time()-time1) //0.006593227386474609
+    future = executor.submit(sum, 1, 10000000)
